c3241c4a2976514bcdbac504fb22a93fb1c0c9ca
AndriyBerezovskiy <andriyxox@gmail.com>
2022-02-16 16:08:01 +0200

    -

--- lesson1/app.js
+++ lesson1/app.js
@@ -44,3 +44,49 @@ const fs = require('fs');
 //     })
 // })
 
+// 3. Створіть папку (можете вручну) напишіть скріпт який створить в ній якись дані (можуть бути нові папки і файли
+//    (в файли запишіть якусь дату) ) і напишіть функцію яка буде зчитувати папку і перевіряти якщо дані які в ній
+//    лежать - це файли тоді вам потрібно їх очистити, але не видаляти, якщо дані - це папки, вам потрібно їх
+//    перейменувати і додати до назви префікс _new
+//
+// fs.mkdir(path.join(__dirname,'common','test-wrap'),{ recursive:true },(err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err;
+//     }
+//
+//     fs.writeFile(path.join(__dirname,'common','test-wrap','test1.txt'),'Some data',(err) => {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     })
+// })
+
+// const fn = () => {
+//   fs.readdir(path.join(__dirname,'common'),(err,data) => {
+//       if (err) {
+//           console.log(err);
+//           throw err;
+//       }
+//       if (path.join(__dirname,'common','test-wrap','test1','test1.txt')) {
+//           fs.truncate(path.join(__dirname,'common','test-wrap','test1','test1.txt'),(err) => {
+//               if (err) {
+//                   console.log(err);
+//                   throw err;
+//               }
+//           })
+//       }
+//       else {
+//           fs.rename(path.join(__dirname,'common','test-wrap','test1'),
+//               path.join(__dirname,'common','_newtest-wrap','_newtest1'),(err) => {
+//                 if (err) {
+//                     console.log(err);
+//                     throw err;
+//                 }
+//           })
+//       }
+//   })
+// }
+//
+// fn();
