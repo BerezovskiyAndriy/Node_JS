8abc2c6fd203ba321d177c3494974594946e5f29
AndriyBerezovskiy <andriyxox@gmail.com>
2022-02-16 15:51:38 +0200

    -

--- lesson1/app.js
+++ lesson1/app.js
@@ -1,77 +1,46 @@
 const path = require('path');
 const fs = require('fs');
 
-// fs.mkdir(path.join(__dirname,'main'),(err) => {
-//     if (err) {
-//         console.log(err);
-//         throw err;
-//     }
-// })
+// 1. Спробуйте створити якийсь файл txt, прочитайте з нього дані і одразу, дані які ви отримали запишіть їх в інший файл,
+//    в вас вийде невеликий callback hell, пізніше я вам покажу
+//    як можна це обійти, але поки зробіть так
 //
-// fs.mkdir(path.join(__dirname,'main','online'),{ recursive: true },(err) => {
+// fs.readFile(path.join(__dirname,'files','test1.txt'),(err,data) => {
 //     if (err) {
 //         console.log(err);
 //         throw err;
 //     }
-// })
 //
-// fs.mkdir(path.join(__dirname,'main','inPerson'),{ recursive: true },(err) => {
+//     fs.appendFile(path.join(__dirname,'files','test2.txt'),data,(err) => {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//     })
+// })
+
+// 2. Створіть файл ( можете вручну ) заповніть його якимись даними
+//    Прочитайте його, скопіюйте всі дані з нього і перенесіть їх в нову папку та файл в ній,
+//    старий файл видаліть після того як все завершиться. Також вийде callback hell
+//
+// fs.readFile(path.join(__dirname,'files','test3.txt'),(err,data) => {
 //     if (err) {
 //         console.log(err);
 //         throw err;
 //     }
+//
+//     fs.writeFile(path.join(__dirname,'public','newTest.txt'),data,(err) => {
+//         if (err) {
+//             console.log(err);
+//             throw err;
+//         }
+//
+//         fs.unlink(path.join(__dirname,'files','test3.txt'),(err) => {
+//             if (err) {
+//                 console.log(err);
+//                 throw err;
+//             }
+//         })
+//     })
 // })
 
-
-const onlineUsers = [
-    {name: 'Andriy', age: 29, city: 'Lviv' },
-    {name: 'Olia', age: 25, city: 'Odessa' }
-];
-const inPersonUsers = [
-    {name: 'Orest', age: 24, city: 'Kyiv' },
-    {name: 'Oksana', age: 26, city: 'Ivano-Frankivsk' }
-];
-
-fs.writeFile(path.join(__dirname,'main','inPerson','inPerson.txt'),
-    `${inPersonUsers.map(person =>
-        `\nNAME: ${person.name} \nAGE: ${person.age} \nCITY: ${person.city}`)}`,
-    (err) => {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-})
-
-fs.writeFile(path.join(__dirname,'main','online','online.txt'),
-    `${onlineUsers.map(users =>
-        `\nNAME: ${users.name} \nAGE: ${users.age} \nCITY: ${users.city}`)}`,
-    (err) => {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-    })
-
-const helper = () => {
-    if (path.join(__dirname,'main','inPerson','inPerson.txt')) {
-        fs.rename(path.join(__dirname,'main','inPerson','inPerson.txt'),
-            path.join(__dirname,'main','online','inPerson.txt'),(err) => {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-        })
-    }
-
-    if (path.join(__dirname,'main','online','online.txt')) {
-        fs.rename(path.join(__dirname,'main','online','online.txt'),
-            path.join(__dirname,'main','inPerson','online.txt'),(err) => {
-                if (err) {
-                    console.log(err);
-                    throw err;
-                }
-            })
-    }
-}
-
-// helper();
